93a86b1163c26763546c74374da9384eca23dee4
Daniel Barutov <barutovdg@gmail.com>
2026-04-24 20:31:32 +0300

    turn on payment_callback

--- src/presentation/api/v1/payment_callback.py
+++ src/presentation/api/v1/payment_callback.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
+from src.application.usecases.order import UpdateOrderUseCase
+from src.presentation.deps.deps import update_order_use_case
 from src.presentation.shemas.callback_payment import PaymentCallbackRequestResponse
 
 router = APIRouter()
 
 
 @router.post("/orders/payment-callback", status_code=status.HTTP_200_OK)
-async def payment_callback(request: PaymentCallbackRequestResponse):
-    # Обновить статус заказа на PAID при успешном платеже
-    # Обновить статус заказа на CANCELLED при неуспешном платеже
-    # Обеспечить идемпотентность обработки callback'ов
-
+async def payment_callback(
+    request: PaymentCallbackRequestResponse,
+    update_order_use_case: UpdateOrderUseCase = Depends(update_order_use_case),
+):
+    await update_order_use_case.execute(request.order_id, request.status)
     print("Получен callback платежа:", request.model_dump(mode="json"))
     return {"message": "Payment callback received"}
